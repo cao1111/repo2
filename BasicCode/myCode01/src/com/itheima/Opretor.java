3a6a75f19814bd5be64d59777a9645ce84b47a87
cao1111 <765706946@qq.com>
2019-09-22 16:39:26 +0800

    修改本地工程添加到git

--- BasicCode/myCode01/src/com/itheima/Opretor.java
+++ BasicCode/myCode01/src/com/itheima/Opretor.java
@@ -14,10 +14,13 @@ public class Opretor {
         int res4 = a / b ;
         //取模
         int res5 = a % b ;
+        //测试
+        int res6 = a + b ;
         System.out.println(res1);
         System.out.println(res2);
         System.out.println(res3);
         System.out.println(res4);
         System.out.println(res5);
+        System.out.println(res6);
     }
 }
